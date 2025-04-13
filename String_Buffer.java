5973aac8c0e66e236f04d75f8e0fea152047d01f
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-13 19:07:11 +0545

    Add files via upload

---
+++ String_Buffer.java
@@ -0,0 +1,53 @@
+// public class String_Buffer {
+
+//     public static void main(String[] args) {
+
+        // String is a unmutable/unchangele so to make the string changable in the same object the string buffer is use.....
+
+        // few methods of string buffer are
+        //a append()     b capacity()     c insert()     d replace()   e delete()     f reverse()
+
+        //a ---->> append()
+
+//        StringBuffer sb = new StringBuffer("hello");
+//        sb.append("  there");
+//        System.out.println(sb);
+
+        //b ------->capacity()
+
+//        StringBuffer sb = new StringBuffer();
+//        sb.capacity();
+//       sb.append("hello");                                                           new capacity = (old capacity*2)+2
+//        System.out.println(sb.capacity());
+//        sb.append("there i am a student");
+//        System.out.println(sb.capacity());
+
+        // insert()
+
+//        StringBuffer sb = new StringBuffer("Mount");
+//        sb.insert(5,"everest");                   // pachau index bata chai given string lai add garxa
+//        System.out.println(sb);
+
+        // replace()
+
+//        StringBuffer sb = new StringBuffer("MOUNT");
+//       sb.replace(1,4,"hello");
+//        System.out.println(sb);
+
+        //delete()
+
+//        StringBuffer sb = new StringBuffer("sasingodar");
+//        sb.delete(0,5);
+//        System.out.println(sb);
+
+
+        //reverse()
+
+//         StringBuffer sb = new StringBuffer("Reverse");
+//         sb.reverse();
+//         System.out.println(sb);
+
+
+
+//     }
+// }
