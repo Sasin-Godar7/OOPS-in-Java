8f0a82c5b8385d5bab1e6a68e323bd4fbf0943f0
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-18 11:28:29 +0545

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,25 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        System.out.println(" Comments");
+
+
+        System.out.println("Single line comment");
+
+        // This is a single line comment contains only single line content
+
+
+        System.out.println("Multi-line comment");
+
+        /* this is a multi line comment which is
+        used to comment the content of more than one line
+         */
+
+
+
+        System.out.println("Documentation comment");
+
+        /** this is a documentation comment which is used to comment the content in a  document form */
+
+    }
+}
