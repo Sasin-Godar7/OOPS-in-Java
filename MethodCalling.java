d64d4de1ac7683607c19ddad6c9bb177e24513e4
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-21 13:03:43 +0545

    Add files via upload

---
+++ MethodCalling.java
@@ -0,0 +1,22 @@
+public class MethodCalling {
+
+    public static void m(){
+        int a= 2;
+        int b = 2;
+        System.out.println(a+b);
+    }
+
+    public static void n(){
+        int c = 69;
+        System.out.println(c);
+    }
+    public static void main(String args[])
+            //akdhewlkdjskkkkkkkkkkkka
+
+    {
+       m();
+       n();
+
+
+    }
+}
