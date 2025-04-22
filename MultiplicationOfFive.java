bd78256f08c7b6ee88fcd4b2395419648b68720d
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-22 12:07:27 +0545

    Add files via upload

---
+++ MultiplicationOfFive.java
@@ -0,0 +1,13 @@
+public class MultiplicationOfFive {
+    public static void main(String[] args) {
+        int n= 5;
+        int i;
+        for(i=1;i<=10 ;i++)
+        {
+            System.out.printf("% d X %d = %d",n,i, n*i);
+            System.out.print("\n");
+        }
+//        System.out.println("\n");
+//        System.out.println(n*i);
+    }
+}
