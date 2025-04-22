d04746fb7d43156ce867e3313c437ee52b9ed53d
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-22 12:08:14 +0545

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,21 @@
+public class OddEven {
+
+    public static void checkOddEven(){
+
+        int a = 6;
+        if(a % 2==0)
+        {
+            System.out.println("even");
+
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        checkOddEven();
+
+    }
+}
