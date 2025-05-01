4e44b5df2e93605da272dd6b2c26e2e631b0898b
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-01 19:31:31 +0545

    Add files via upload

---
+++ Sivadarsini.java
@@ -0,0 +1,10 @@
+public class Sivadarsini {
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++)
+        {
+            System.out.println("i see my self");
+        }
+        System.out.println("in the mirror");
+    }
+}
+
