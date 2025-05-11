f1aea720f0528e85580ba5a5abe478a96ad59df0
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 12:24:15 +0545

    push

---
+++ WrapperClassInJava/JavaStackExample.java
@@ -0,0 +1,18 @@
+package WrapperClassInJava;
+import java.util.*;
+
+public class JavaStackExample {
+    public static void main(String[] args) {
+
+        Stack<Integer>stk = new Stack<>();     // stack create gareko
+
+        //checking if the stack is empty or not
+        boolean result = stk.empty();
+        System.out.println("Stack is empty ?? " + result);
+
+        // if not empty now pushing the element in  the stack
+        
+
+
+    }
+}
