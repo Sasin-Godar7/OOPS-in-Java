6485b6452d104a2a836de8630043eda864687002
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 11:34:34 +0545

    update

---
+++ Collections/LinkedListExample.java
@@ -0,0 +1,17 @@
+package Collections;
+import java.util.*;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+
+        LinkedList<String>ll = new LinkedList<>();     // linked list ko object create gareko
+
+        ll.add("Sasin");
+        ll.add("Prabin");
+        ll.add("Anuzz");
+
+        
+
+
+    }
+}
