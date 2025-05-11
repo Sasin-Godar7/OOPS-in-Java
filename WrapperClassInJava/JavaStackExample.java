ddcbfdb6c2780b79b3a7062ae7424883492f8c58
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 12:28:10 +0545

    stack

--- WrapperClassInJava/JavaStackExample.java
+++ WrapperClassInJava/JavaStackExample.java
@@ -11,7 +11,19 @@ public class JavaStackExample {
         System.out.println("Stack is empty ?? " + result);
 
         // if not empty now pushing the element in  the stack
-        
+
+        stk.push(101);
+        stk.push(69);
+        stk.push(102);
+        stk.push(7);
+
+        // printing the elements from the stack
+
+        System.out.println("Elements in stack are :" + stk);
+
+      result = stk.empty();
+
+        System.out.println("nothing left in the stack ..stack is empty" + result);
 
 
     }
