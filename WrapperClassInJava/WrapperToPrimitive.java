59b7bd6cf7138ab979090e38536a31f4d3795b19
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 12:11:58 +0545

    ert

---
+++ WrapperClassInJava/WrapperToPrimitive.java
@@ -0,0 +1,16 @@
+package WrapperClassInJava;
+
+public class WrapperToPrimitive {
+    public static void main(String[] args) {
+
+        //unboxing == it is the reverse process of the autoboxing ..automaticaly conerting the object of the
+        //wrapper class into its corresponding primitive value.
+
+        Integer a = new Integer(3);
+
+        int i = a.intValue();   // converting the integer value i to int
+
+
+
+    }
+}
