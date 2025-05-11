2cac174ac546cb39f63de2a064e024f032059673
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 11:44:08 +0545

    Treeset

--- Collections/TreeSetExample.java
+++ Collections/TreeSetExample.java
@@ -1,10 +1,23 @@
 package Collections;
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 public class TreeSetExample {
     public static void main(String[] args) {
 
-        
+        TreeSet<String>set = new TreeSet<>();
+
+        set.add("Sasin");
+        set.add("Anuzz");
+        set.add("Prabin");
+
+        Iterator itr = set.iterator();
+
+        while(itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
 
     }
 }
