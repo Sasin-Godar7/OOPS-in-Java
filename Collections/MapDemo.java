08ad59d54bebf65bd22993732c826f44bcc66aa6
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 11:55:41 +0545

    update

--- Collections/MapDemo.java
+++ Collections/MapDemo.java
@@ -4,6 +4,16 @@ import java.util.*;
 public class MapDemo {
     public static void main(String[] args) {
 
-        
+        Map<Integer,String>map = new HashMap<>();
+
+        map.put(101,"Sasin");
+        map.put(102,"Ashok");
+        map.put(103,"Rajan");
+
+        for(Map.Entry m:map.entrySet()){
+
+            System.out.println(m.getKey() + " " + m.getValue());
+
+        }
     }
 }
