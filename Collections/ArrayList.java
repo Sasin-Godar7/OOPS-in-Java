b74f8cf12ba498bfa185016c54901ab4b2073c7d
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-11 11:22:29 +0545

    upd

--- Collections/ArrayList.java
+++ Collections/ArrayList.java
@@ -1,20 +1,20 @@
 package Collections;
+
 import java.util.*;
 
-        public class ArrayList {
-            public static void main(String[] args) {
-                // List banauxa
-                ArrayList<String> fruits = new ArrayList<>();
+public class ArrayList {
+    public static void main(String[] args) {
+        // List banauxa
+        ArrayList<String> fruits = new ArrayList<>();
 
-                // Add gareko
-                fruits.add("Apple");
-                fruits.add("Banana");
-                fruits.add("Mango");
+        // Add gareko
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
 
-                // Print gareko
-                System.out.println(fruits);
-            }
-        }
-
+        // Print gareko
+        System.out.println(fruits);
     }
 }
+
+
