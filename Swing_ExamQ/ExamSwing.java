290f57beaa51db5d30f3e5febc9105123c1be11b
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-23 15:49:52 +0545

    uo

--- Swing_ExamQ/ExamSwing.java
+++ Swing_ExamQ/ExamSwing.java
@@ -1,4 +1,5 @@
 package Swing_ExamQ;
+import javax.management.modelmbean.ModelMBeanInfo;
 import javax.swing.*;
 
 public class ExamSwing {
@@ -6,6 +7,14 @@ public class ExamSwing {
     public ExamSwing()
     {
 
-        
+        JFrame f = new JFrame("Exam Question");
+        f.setSize(300,300);
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      
+
+
     }
 }
