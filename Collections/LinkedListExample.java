ae6501dc6fca174e916380cf5fed54efbaed93e2
Sasin Godar <sago_bca2080@lict.edu.np>
2025-05-23 20:01:57 +0545

    hj

--- Collections/LinkedListExample.java
+++ Collections/LinkedListExample.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class LinkedListExample {
     public static void main(String[] args) {
 
-        LinkedList<String>ll = new LinkedList<>();     // linked list ko object create gareko
+        LinkedList<String>ll = new LinkedList<>();     // linked list ko obje
+        // ct create gareko
 
         ll.add("Sasin");
         ll.add("Prabin");
--- Collections/MapDemo.java
+++ Collections/MapDemo.java
@@ -15,5 +15,7 @@ public class MapDemo {
             System.out.println(m.getKey() + " " + m.getValue());
 
         }
+
+
     }
 }
