6470801b21f4190ae2be5c2c58ec23f171a60a55
Sasin Godar <sago_bca2080@lict.edu.np>
2025-07-07 14:59:58 +0545

    update

--- filehandeling_CharOriented/PrimeCompCheck.java
+++ filehandeling_CharOriented/PrimeCompCheck.java
@@ -1,56 +1,50 @@
 package filehandeling_CharOriented;
 import java.util.Scanner;
 import java.io.FileWriter;
-import java. io. IOException;
-import java.io.*;
 
 public class PrimeCompCheck {
     public static void main(String[] args) {
         try {
-            FileWriter prime = new FileWriter("prime.txt");         // Prime number ko file banaune
-            FileWriter comp = new FileWriter("composite.txt");      // Composite number ko file banaune
+            FileWriter prime = new FileWriter("prime.txt");
+            FileWriter comp = new FileWriter("composite.txt");
 
             Scanner sc = new Scanner(System.in);
             int[] a = new int[5];
-            int num=0;
 
-
-            System.out.println("enter the numbers to check :");
+            System.out.println("Enter 5 numbers:");
             for (int i = 0; i < 5; i++) {
                 a[i] = sc.nextInt();
             }
 
             for (int i = 0; i < 5; i++) {
-                num = a[i];
+                int num = a[i];
+                int count = 0;
 
                 if (num <= 1) {
-                    System.out.println(num + "" + " is neither prime nor composite");
+                    System.out.println(num + " is neither prime nor composite");
+                    continue;
+                }
+
+                for (int j = 2; j < num; j++) {
+                    if (num % j == 0) {
+                        count++;
+                    }
+                }
+
+                if (count == 0) {
+                    prime.write(num + "\n");
+                    System.out.println(num + " => prime file ma saved");
+                } else {
+                    comp.write(num + "\n");
+                    System.out.println(num + " => composite file ma saved");
                 }
             }
 
-            int count = 0;
-
-            for (int j = 2; j < num; j++) {
-                if (num % j == 0) {
-                    count++;
-                }
-            }
-
-            if (count == 2) {
-                prime.write(num);
-                System.out.println(num +"" + "=> prime file ma saved");
-            }
-            else
-            {
-                comp.write(num);
-                System.out.println(num +"" + "=> composite file ma saved");
-            }
             prime.close();
             comp.close();
 
-
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
